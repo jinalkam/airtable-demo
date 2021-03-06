8091bb823e99f17a64c82894236183f6e022cef2
Jinal <er.jinalkamdar@gmail.com>
2021-03-06 17:34:34 +0530

    Added create user feature

--- app/Http/Controllers/PeopleController.php
+++ app/Http/Controllers/PeopleController.php
@@ -10,13 +10,39 @@ class PeopleController extends Controller
 
 	public function index()
 	{ 
-		
-		$data = \Cache::remember('users', 10, function () {
+		$data = \Cache::remember('users', 30, function () {
 			return Airtable::table('default')->all();
 		});
-		return view('people', ['data' => $data]);
+		return view('people', ['users' => $data]);
 	}
 
 
 
+	public function store(Request $request)
+	{ 
+
+		$validator = Validator::make($request->all(), [
+			'name' => 'required',
+			'email' => 'required|email',
+		]);
+
+		if ($validator->fails()) {
+			return response()->json(['error' => $validator->errors()], 401);
+		}
+
+		$getResult = Airtable::table('default')->create([
+		'Name' => $request->name,
+		'Email' => $request->email,
+		]);
+
+		if($request->file('photo')) {
+			 $file_url = cloudinary()->upload($request->file('photo')->getRealPath())->getSecurePath();
+			 $getResult = Airtable::table('default')->patch($getResults['id'], ['Photo' => [['url' => $file_url]] ]);
+			 
+		}
+        return response()->json([
+			'message' => 'User added successfully',
+			'user' => $getResult
+		   ]);
+	}
 }
--- resources/js/components/PeopleComponent.vue
+++ resources/js/components/PeopleComponent.vue
@@ -4,94 +4,128 @@
     <div class="grid grid-cols-2 gap-20">
       <div>
         <h2 class="text-xl mb-5">Add new team member</h2>
-        <form class="space-y-5" enctype="multipart/form-data">
-          <div>
-            <label for="name" class="block text-sm font-medium text-gray-700"
-              >Name</label
-            >
-            <div class="mt-1">
-              <input
-                v-model="form.name"
-                type="text"
-                name="name"
-                id="name"
-                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 p-3 border rounded-md"
-                placeholder="Calvin Hawkins"
-              />
-            </div>
-          </div>
-          <div>
-            <label for="email" class="block text-sm font-medium text-gray-700"
-              >Email</label
-            >
-            <div class="mt-1">
-              <input
-                v-model="form.email"
-                type="text"
-                name="email"
-                id="email"
-                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 p-3 border rounded-md"
-                placeholder="you@example.com"
-              />
-            </div>
-          </div>
-          <div>
-            <label
-              for="photo"
-              class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
-            >
-              Photo
-            </label>
-            <div class="mt-1">
-              <div
-                class="w-full flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md"
+        <ValidationObserver v-slot="{ handleSubmit }">
+          <form
+            class="space-y-5"
+            @submit.stop.prevent="handleSubmit(onSubmit)"
+            enctype="multipart/form-data"
+          >
+            <div>
+              <label for="name" class="block text-sm font-medium text-gray-700"
+                >Name</label
               >
-                <div class="space-y-1 text-center">
-                  <svg
-                    class="mx-auto h-12 w-12 text-gray-400"
-                    stroke="currentColor"
-                    fill="none"
-                    viewBox="0 0 48 48"
-                    aria-hidden="true"
-                  >
-                    <path
-                      d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                    />
-                  </svg>
-                  <div class="flex text-sm text-gray-600">
-                    <label
-                      for="file-upload"
-                      class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                    >
-                      <span>Upload a file</span>
-                      <input
-                        v-on:change="onImageChange"
-                        id="file-upload"
-                        name="file-upload"
-                        type="file"
-                        class="sr-only"
-                      />
-                    </label>
-                    <p class="pl-1">or drag and drop</p>
-                  </div>
-                  <p class="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-                </div>
+              <div class="mt-1">
+                <ValidationProvider
+                  name="Name"
+                  rules="required"
+                  v-slot="{ errors }"
+                >
+                  <input
+                    v-model="form.name"
+                    type="text"
+                    name="fullname"
+                    id="fullname"
+                    class="form-control shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 p-3 border rounded-md "
+                    :class="{ 'error-input': errors[0] }"
+                    placeholder="Calvin Hawkins"
+                  />
+                  <span :class="{ 'error-message': errors[0] }">{{ errors[0] }}</span>
+                </ValidationProvider>
               </div>
             </div>
-          </div>
-          <button
-            class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Submit
-          </button>
-        </form>
+            <div>
+              <label for="email" class="block text-sm font-medium text-gray-700"
+                >Email</label
+              >
+              <div class="mt-1">
+                <ValidationProvider
+                  name="E-mail"
+                  rules="required|email"
+                  v-slot="{ errors }"
+                >
+                  <input
+                    v-model="form.email"
+                    type="text"
+                    name="email"
+                    id="email"
+                    class="form-control shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 p-3 border rounded-md"
+                    :class="{ 'error-input': errors[0] }"
+                    placeholder="you@example.com"
+                  />
+                             <span :class="{ 'error-message': errors[0] }">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </div>
+            </div>
+            <div>
+              <label
+                for="photo"
+                class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+              >
+                Photo
+              </label>
+              <div class="mt-1">
+                      <ValidationProvider
+                  name="file-upload"
+                  rules="required|mimes:image/jpeg,image/png"
+                  v-slot="{ errors }"
+                >
+                <div
+                  class="w-full flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md"
+                >
+                  <div class="space-y-1 text-center">
+                    <svg
+                      class="mx-auto h-12 w-12 text-gray-400"
+                      stroke="currentColor"
+                      fill="none"
+                      viewBox="0 0 48 48"
+                      aria-hidden="true"
+                    >
+                      <path
+                        d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                        stroke-width="2"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                    <div class="flex text-sm text-gray-600">
+                      <label
+                        for="file-upload"
+                        class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                      >
+                        <span>Upload a file</span>
+                    
+                        <input
+                          v-on:change="onImageChange"
+                          id="file-upload"
+                          name="file-upload"
+                          type="file"
+                          class="sr-only"
+                        />  
+                          
+                      </label>
+                      <p class="pl-1">or drag and drop</p>
+                    </div>
+                    <p class="text-xs text-gray-500">
+                      PNG, JPG, GIF up to 10MB
+                    </p>
+                  </div>
+                </div>
+               <span :class="{ 'error-message': errors[0] }" >{{ errors[0] }}</span>
+                 </ValidationProvider>
+              </div>
+            </div>
+            <button
+              type="submit"
+              class="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Submit
+            </button>
+          </form>
+        </ValidationObserver>
       </div>
       <div>
         <ul class="divide-y divide-gray-200">
-          <li class="py-4 flex" v-for="(value, key) in data" :key="key">
+          <li class="py-4 flex" v-for="(value, key) in usersList" :key="key">
             <img
               v-if="value.fields.Photo"
               class="h-10 w-10 rounded-full"
@@ -112,15 +146,29 @@
 </template>
 
 <script>
+import { ValidationProvider, ValidationObserver } from "vee-validate";
+import { extend } from "vee-validate";
+import * as rules from "vee-validate/dist/rules";
+
+Object.keys(rules).forEach((rule) => {
+  extend(rule, rules[rule]);
+});
+
 export default {
+  components: {
+    ValidationProvider,
+    ValidationObserver,
+  },
+
   props: {
-    data: {
+    users: {
       type: Array,
-      default: [],
+      default: () => [],
     },
   },
   data() {
     return {
+      usersList: [],
       form: {
         name: null,
         email: null,
@@ -128,13 +176,46 @@ export default {
       },
     };
   },
+
   mounted() {},
-  watch: {},
+  watch: {
+    users: {
+      immediate: true,
+      handler(data) {
+        this.usersList = data;
+      },
+    },
+  },
+
   methods: {
     onImageChange(e) {
-      console.log(e.target.files[0]);
       this.form.photo = e.target.files[0];
+    },
+    async onSubmit(e) {
+      let formData = new FormData();
+      const config = {
+        headers: { "content-type": "multipart/form-data" },
+      };
+      formData.append("photo", this.form.photo);
+      formData.append("name", this.form.name);
+      formData.append("email", this.form.email);
+      const { data } = await window.axios.post("/api/user", formData, config);
+      if (data) {
+        this.usersList.unshift(data.user);
+      }
     },
   },
 };
 </script>
+
+<style lang="css" scoped>
+.error-input {
+  border-color: #dc3545;
+}
+span.error-message {
+  width: 100%;
+  margin-top: .25rem;
+  font-size: 80%;
+  color: #dc3545;
+}
+</style>
